c1048e2505a984f1a0061892b8738d1dfac10b25
Numan Korkmaz <heysemkorkmaz@gmail.com>
2020-11-17 18:27:00 +0100

    deleted the dummy data

--- src/Main.java
+++ src/Main.java
@@ -6,23 +6,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        /*Controller start = Controller.getInstance();
-        start.run();*/
-
-        ProjectLibrary projects = new ProjectLibrary();
-        Project projectX = projects.createProject("Dummy Duck","Inject Adamentium to the Wolverine");
-        System.out.println("Project with ID: "+ projectX.getID()+ "has been created successfully");
-        System.out.println("list of all projects");
-        System.out.println(projects.listAll());
-
-        if (projects.findItInList(projectX.getID()) != null ){
-            System.out.println("Yes, the project x exist in the list");
-        }
-
-        if(projects.deleteProject(projectX.getID())){
-            System.out.println("Project has been deleted successfully");
-        }
-        System.out.println("list of all projects");
-        System.out.println(projects.listAll());
+        Controller start = Controller.getInstance();
+        start.run();
     }
 }
--- src/entities/ProjectLibrary.java
+++ src/entities/ProjectLibrary.java
@@ -3,15 +3,4 @@ package entities;
 //please dont edit it yet
 public class ProjectLibrary extends DataLibrary{
 
-    public Project createProject(String projectOwner, String task){
-        Project newProj = new Project(projectOwner, task);
-        addToList(newProj);
-        return newProj;
-    }
-
-    public boolean deleteProject(String idToDelete){
-        return removeItFromList(idToDelete);
-    }
-
-
 }
