f68c01e2f956815d10a4164e137769010faf19a9
JensTheS <jeens93@gmail.com>
2020-11-17 19:40:02 +0100

    Added attributes to Project.java

--- src/entities/Project.java
+++ src/entities/Project.java
@@ -5,7 +5,7 @@ package entities;
 public class Project extends Data{
     // Data.projectID
     private String name;
-    private User owner;
+    private String owner; // Change to User
     // private Team team;
     // private TaskList taskList;
     // private createdDate;
@@ -14,12 +14,14 @@ public class Project extends Data{
     private String description;
     private String status; // Active/Inactive
     
-    public Project(String name, User owner, String createdDate, String startDate) {
+    public Project(String name, String owner, String startDate, String endDate) {
     }
     
     public int duration() {
         // startDate - endDate;
         return 1;
     }
+    
+    // Getters & Setters
     
 }
