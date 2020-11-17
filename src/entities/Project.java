5c413e2ff3a1c6f2fae35b60d380dccccf33a936
JensTheS <jeens93@gmail.com>
2020-11-17 19:00:28 +0100

    Added attributes to Project.java

--- src/entities/Project.java
+++ src/entities/Project.java
@@ -1,6 +1,25 @@
 package entities;
-//currently just for testing Data, not the final class
-//please dont edit it yet
-public class Project extends Data{
 
+// import date time for calculating Duration
+
+public class Project extends Data{
+    // Data.projectID
+    private String name;
+    private User owner;
+    // private Team team;
+    // private TaskList taskList;
+    // private createdDate;
+    // private startDate;
+    // private endDate;
+    private String description;
+    private String status; // Active/Inactive
+    
+    public Project(String name, User owner, String createdDate, String startDate) {
+    }
+    
+    public int duration() {
+        // startDate - endDate;
+        return 1;
+    }
+    
 }
