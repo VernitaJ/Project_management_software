f8f984be008c66f42c93be0619ff35b972b0c07c
JensTheS <jeens93@gmail.com>
2020-11-22 15:40:20 +0100

    Added getters and setters. Kept some parts commented due to uncertainty of other User Stories build.

--- src/entities/Task.java
+++ src/entities/Task.java
@@ -1,5 +1,7 @@
 package entities;
 
+import tools.Input;
+
 public class Task extends Data{
     private User createdBy;
     // User assignedTo; //we can create a new class extends DataLibrary
@@ -13,6 +15,63 @@ public class Task extends Data{
         this.name = name;
         this.description = description;
         this.status = "Default";
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public String getStatus() {
+        return status;
+    }
+    
+    public User getAssignees() {
+        return assignees;
+    }
+    
+    public User getCreatedBy() {
+        return createdBy;
+    }
+    
+    private void setName(String name) {
+        this.name = name;
+    }
+    
+    private void setDescription(String description) {
+        this.description = description;
+    }
+    
+    private void setStatus(String status) {
+        this.status = status;
+    }
+    
+    private void setAssignees(User assignees) {
+        this.assignees = assignees;
+    }
+    
+    public void updateName() {
+        Input input = Input.getInstance();
+        // if(role == ?)
+        String newStatus = input.getStr("Enter the name: ");
+    }
+    
+    public void updateDescription() {
+        Input input = Input.getInstance();
+        // if(role == ?)
+        String newStatus = input.getStr("Enter the description: ");
+    }
+    
+    public void setStatus() {
+        // Waiting to see if status is an object etc..
+    }
+    
+    public void updateAssignees() {
+        // if(role == ?)
+        // Find by id etc.
     }
     
     //public assign(){}
