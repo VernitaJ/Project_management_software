8abb4d0cdb9025f646e1a065bda41170ddd1ede9
JensTheS <jeens93@gmail.com>
2020-11-22 15:30:12 +0100

    Added default status for now. Preparing for assigning users to task.

--- src/entities/Task.java
+++ src/entities/Task.java
@@ -1,36 +1,35 @@
 package entities;
 
-import java.util.ArrayList;
-
 public class Task extends Data{
     private User createdBy;
     // User assignedTo; //we can create a new class extends DataLibrary
-    private String status; //NotStarted/Ongoing/Completed
-
+    private String status; //NotStarted/Ongoing/Completed?
     private String name;
     private String description;
-
+    private User assignees;
+    
     public Task(User createdBy, String name, String description){
-        this.createdBy=createdBy;
-        this.name=name;
-        this.description=description;
+        this.createdBy = createdBy;
+        this.name = name;
+        this.description = description;
+        this.status = "Default";
     }
-
+    
     //public assign(){}
-
+    
     //public changeStatus(){}
-
+    
     //public changeTitle(){}
-
-    //public changeBody(){}
-
+    
+    //public changeDescription(){}
+    
     //getters
     //setter
-
-
-
-
-
-
-
+    
+    
+    
+    
+    
+    
+    
 }
