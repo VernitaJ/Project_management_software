2505b526badeee961b7800824b09854b14e1766d
VernitaJ <vernitajones87@gmail.com>
2020-11-24 09:58:27 +0100

    UserName check added. Function name changed as to not overwrite parent class function.

--- src/entities/ProjectLibrary.java
+++ src/entities/ProjectLibrary.java
@@ -1,6 +1,17 @@
 package entities;
-//currently just for testing DataLibrary, not the final class
-//please dont edit it yet
-public class ProjectLibrary extends DataLibrary{
 
+import tools.Input;
+
+import java.time.LocalDate;
+
+public class ProjectLibrary extends DataLibrary{
+    //user control is missing
+    public void createProject() {
+    }
+
+    //access level and ownership check is missing
+    public boolean deleteProject(String idToDelete){
+        return removeItFromList(idToDelete);
+    }
+    
 }
