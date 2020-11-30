b2e608e669a24cbcf9ce216e7229ad26b8bfd057
ktpa <itkalle@outlook.com>
2020-11-30 15:30:09 +0100

    Created the tracking package and classes for 11.

---
+++ src/tracking/ProjectCost.java
@@ -0,0 +1,11 @@
+package tracking;
+import budget.Budget;
+
+
+public class ProjectCost {
+
+    public void viewCost() {
+
+    }
+
+}
---
+++ src/tracking/ProjectHours.java
@@ -0,0 +1,10 @@
+package tracking;
+
+public class ProjectHours {
+
+    public float getHours() {
+        float hours = 0;
+        return(hours);
+    }
+
+}
