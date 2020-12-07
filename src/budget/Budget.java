696bcf0718219e2049f18d2dbf470ae01a30ec35
JensTheS <jeens93@gmail.com>
2020-12-07 16:57:45 +0100

    Added values to check if budget exists.

--- src/budget/Budget.java
+++ src/budget/Budget.java
@@ -7,39 +7,52 @@ public class Budget
     private double money;
     private double hours;
     public final String CURRENCY = "SEK";
-
+    
     public Budget(){
         this.money = 0.0;
         this.hours = 0.0;
     }
-
+    
     public Budget(double money, double hours){
         this.money = money;
         this.hours = hours;
     }
-
+    
     public void calculation(Project currentProject){
         if (currentProject.getBudget().getMoney() != 0) {
         } else System.out.println("Sorry, no budget has been added. chat to PM");
     }
-
+    
     public float timeLeftBeforeExceedingBudget() {
         return 0.0f;
     }
-
+    
     public double getMoney() {
         return money;
     }
-
+    
     public void setMoney(double money) {
         this.money = money;
     }
-
+    
     public double getHours() {
         return hours;
     }
-
+    
     public void setHours(double hours) {
         this.hours = hours;
+    }
+    
+    public boolean budgetExistMoney() {
+        if(this.getMoney() > 0) {
+            return true;
+        }
+        return false;
+    }
+    public boolean budgetExistHours() {
+        if(this.getHours() > 0) {
+            return true;
+        }
+        return false;
     }
 }
