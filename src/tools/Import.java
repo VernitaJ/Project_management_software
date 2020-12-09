749ee070d0661476192111d989dc9febf5d3af32
ktpa <itkalle@outlook.com>
2020-12-09 15:02:01 +0100

    Added the Apache POI library to ease the import of excel files.

--- src/tools/Import.java
+++ src/tools/Import.java
@@ -1,8 +1,52 @@
 package tools;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+
+import java.io.FileInputStream;
+
 public class Import {
-    
+
     public Import() {
+        try {
+            POIFSFileSystem fs = new POIFSFileSystem(new FileInputStream(file));
+            HSSFWorkbook wb = new HSSFWorkbook(fs);
+            HSSFSheet sheet = wb.getSheetAt(0);
+            HSSFRow row;
+            HSSFCell cell;
+
+            int rows; // No of rows
+            rows = sheet.getPhysicalNumberOfRows();
+
+            int cols = 0; // No of columns
+            int tmp = 0;
+
+            // This trick ensures that we get the data properly even if it doesn't start from first few rows
+            for(int i = 0; i < 10 || i < rows; i++) {
+                row = sheet.getRow(i);
+                if(row != null) {
+                    tmp = sheet.getRow(i).getPhysicalNumberOfCells();
+                    if(tmp > cols) cols = tmp;
+                }
+            }
+
+            for(int r = 0; r < rows; r++) {
+                row = sheet.getRow(r);
+                if(row != null) {
+                    for(int c = 0; c < cols; c++) {
+                        cell = row.getCell((short)c);
+                        if(cell != null) {
+                            // Your code here
+                        }
+                    }
+                }
+            }
+        } catch(Exception ioe) {
+            ioe.printStackTrace();
+        }
     }
     
     // IMPORT
@@ -18,10 +62,12 @@ public class Import {
             // Worklog
     
         // Read JSON output file into system
-            // Only User doing import can become the owner.
+            // Check encrypted string for match with encrypted username of user importing.
+            // If no string exists for the owner, boolean ownerExists is set to false.
     
     // EXPORT
-        // Check user access
+        // If ownerExists is false, create a new row in JSON file with the username string encrypted.
+        // Check user access against owner of JSON file.
         // Generate JSON output file
         
     
