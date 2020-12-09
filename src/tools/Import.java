317e7d7f5be88ac242b3dedd582d991bea5ac422
JensTheS <jeens93@gmail.com>
2020-12-09 14:31:57 +0100

    wrote some pseudocode

--- src/tools/Import.java
+++ src/tools/Import.java
@@ -1,2 +1,29 @@
-package tools;public class Import {
+package tools;
+
+public class Import {
+    
+    public Import() {
+    }
+    
+    // IMPORT
+        // Read data from excel
+        // Excel location in user home folder
+        // Read all data
+        // Generate JSON output file
+        
+        // JSON File Structure
+            // User
+            // Project
+            // Task
+            // Worklog
+    
+        // Read JSON output file into system
+            // Only User doing import can become the owner.
+    
+    // EXPORT
+        // Check user access
+        // Generate JSON output file
+        
+    
+    // projekt:A, desc:rädda världen, start:2020-12-09 end:2020-12-12
 }
