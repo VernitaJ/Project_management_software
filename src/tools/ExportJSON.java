628fc992e28b105d3ec2fc9c243550b0fdb64ea4
ktpa <itkalle@outlook.com>
2020-12-18 15:43:08 +0100

    Fixed the MAPPER code

--- src/tools/ExportJSON.java
+++ src/tools/ExportJSON.java
@@ -1,6 +1,5 @@
 package tools;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import entities.*;
@@ -9,8 +8,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.List;
-import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 
 
@@ -18,58 +16,52 @@ public class ExportJSON {
     private final ProjectLibrary projectLibrary;
     private final TaskLibrary taskLibrary;
     private final UserLibrary userLibrary;
+    private final ObjectMapper MAPPER;
     private String path;
 
     public ExportJSON(ProjectLibrary projectLibrary, TaskLibrary taskLibrary, UserLibrary userLibrary) throws IOException {
         this.projectLibrary = projectLibrary;
         this.taskLibrary = taskLibrary;
         this.userLibrary = userLibrary;
+        this.MAPPER = new ObjectMapper();
+        this.MAPPER.registerModule(new JavaTimeModule());
+        this.MAPPER.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        this.MAPPER.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         this.path = System.getProperty("user.home") + "/simpledirection.json";
+
         collectData();
         writeJSON();
+        readJSON();
     }
 
     public void writeJSON() throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JSR310Module());
-        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        mapper.writerWithDefaultPrettyPrinter().writeValue(Paths.get(this.path).toFile(), this.projectLibrary.getDataList());
+        MAPPER.writerWithDefaultPrettyPrinter().writeValue(Paths.get(this.path).toFile(), this.projectLibrary.getDataList());
     }
 
     public void readJSON() throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JSR310Module());
-        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        //MyValue older = mapper.readValue(new File("my-older-stuff.json"), MyValue.class);
+        //ArrayList<String> pogger = MAPPER.readValue(new File(this.path), ArrayList.class);
+        // project = MAPPER.readValue(new File(this.path), Project.class);
+        this.projectLibrary.addProjectToList(MAPPER.readValue(new File(this.path), Project.class));
+        //System.out.println(project.toString());
+        //for(int i = 0; i < pogger.size(); i++) {
 
-        // // Or if you prefer JSON Tree representation:
-        //  JsonNode root = mapper.readTree(newState);
-        //  // and find values by, for example, using a JsonPointer expression:
-        //  int age = root.at("/personal/age").getValueAsInt();
-
-        ArrayList<String> pogger = mapper.readValue(new File(this.path), ArrayList.class);
-        for(int i = 0; i < pogger.size(); i++) {
-
-        }
-        System.out.println(pogger.toString());
+        //}
+        //System.out.println(pogger.toString());
     }
 
-    public List<Data> fetchAllData() {
-        List filledList = new ArrayList<>();
-
-        filledList.addAll(this.userLibrary.getAllUsers());
-        filledList.addAll(this.projectLibrary.getDataList());
-
-        //filledList.addAll(this.taskLibrary.getDataList());
 
 
-        return(filledList);
-    }
 
-    // newline %x0A /
-    // tab %x09 /
+
+
+
+
+
+
+
+
+
+
 
     private void collectData() {
 
